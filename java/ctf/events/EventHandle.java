e7439b1265a60b9435b48414eeeba55c07dd8fdc
alexdemeo <alex.demeo129@gmail.com>
2016-05-15 20:03:56 -0400

    "test"

--- java/ctf/events/EventHandle.java
+++ java/ctf/events/EventHandle.java
@@ -1,21 +1,29 @@
 package ctf.events;
 
+import java.util.Iterator;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
 import ctf.blocks.BlockFlag;
 import ctf.blocks.BlockPedestal;
 import ctf.blocks.BlockStand;
 import ctf.blocks.tileentity.PedestalTileEntity;
 import ctf.main.Main;
 import ctf.main.Things;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import net.minecraftforge.event.entity.item.ItemTossEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.world.BlockEvent;
 
 public class EventHandle {
-	
+
 	@SubscribeEvent
 	public void onBlockPlaced(BlockEvent.PlaceEvent evt) {
 		if ((evt.y < 50 || evt.y > 128) && (evt.block instanceof BlockPedestal || evt.block instanceof BlockStand)) {
@@ -25,19 +33,19 @@ public class EventHandle {
 			evt.setCanceled(true);
 		}
 		blockCheck:
-		if (evt.block instanceof BlockStand) {
-			for (int i = evt.x - 5; i <= evt.x + 5; i++) {
-				for (int k = evt.z - 5; k <= evt.z + 5; k++) {
-					if (evt.world.getBlock(i, evt.y, k) instanceof BlockPedestal) {
-						break blockCheck;
+			if (evt.block instanceof BlockStand) {
+				for (int i = evt.x - 5; i <= evt.x + 5; i++) {
+					for (int k = evt.z - 5; k <= evt.z + 5; k++) {
+						if (evt.world.getBlock(i, evt.y, k) instanceof BlockPedestal) {
+							break blockCheck;
+						}
 					}
 				}
+				evt.setCanceled(true);
+				Main.instance.sendPlayerMessage(evt.player, "Stands must be within 5 blocks of a pedestal");
 			}
-			evt.setCanceled(true);
-			Main.instance.sendPlayerMessage(evt.player, "Stands must be within 5 blocks of a pedestal");
-		}
 	}
-	
+
 	@SubscribeEvent
 	public void onBlockBroken(BlockEvent.BreakEvent evt) {
 		if (evt.block instanceof BlockPedestal && !evt.getPlayer().capabilities.isCreativeMode) {
@@ -48,7 +56,7 @@ public class EventHandle {
 			evt.world.setBlockToAir(evt.x, evt.y + 1, evt.z);
 		}
 	}
-	
+
 	@SubscribeEvent
 	public void onItemDropped(ItemTossEvent evt) {
 		Item i = evt.entityItem.getEntityItem().getItem();
@@ -57,17 +65,50 @@ public class EventHandle {
 			evt.player.inventory.addItemStackToInventory(new ItemStack(i));
 		}
 	}
-	
+
 	private int announce = 0;
 	@SubscribeEvent
 	public void onEntityUpdate(LivingUpdateEvent evt) {
 		if (evt.entityLiving instanceof EntityPlayer) {
 			announce++;
 			EntityPlayer player = (EntityPlayer)evt.entityLiving;
-			if (announce >= 200 && player.inventory.hasItem(Item.getItemFromBlock(Things.flag))) {
+			if (announce >= 1200 && player.inventory.hasItem(Item.getItemFromBlock(Things.flag))) {
 				announce = 0;
 				Main.instance.announceMessage(player.getDisplayName() + " has a flag at x=" + (int)player.posX + " z=" + (int)player.posZ);
 			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerJoined(PlayerLoggedInEvent evt) {
+		System.out.println("JOIN");
+		Iterator i = MinecraftServer.getServer().getConfigurationManager().playerEntityList.iterator();
+		while (i.hasNext()) {
+			EntityPlayer p = (EntityPlayer)i.next();
+			if (p.inventory.hasItem(Item.getItemFromBlock(Things.flag))) {
+				Main.instance.sendPlayerMessage(evt.player, p.getDisplayName() + " has a flag");
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerLeft(PlayerLoggedOutEvent evt) {
+		System.out.println("LOGOUT");
+		if (evt.player.inventory.hasItem(Item.getItemFromBlock(Things.flag))) {
+			System.out.println("WITH FLAG");
+			evt.player.inventory.consumeInventoryItem(Item.getItemFromBlock(Things.flag));
+			evt.player.worldObj.spawnEntityInWorld(new EntityItem(evt.player.worldObj, evt.player.posX, evt.player.posY, evt.player.posZ, new ItemStack(Things.flag)));
+			Main.instance.announceMessage(evt.player.getDisplayName() + " has logged out with a flag, it has dropped at (" 
+					+ evt.player.posX 
+					+ " " + evt.player.posY 
+					+ " " + evt.player.posZ + ")");
+		}
+	}
+
+	@SubscribeEvent
+	public void onItemDespawn(ItemExpireEvent evt) {
+		if (evt.entityItem.getEntityItem().getItem() == Item.getItemFromBlock(Things.flag)) {
+			evt.setCanceled(true);
 		}
 	}
 }
